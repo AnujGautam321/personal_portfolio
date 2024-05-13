10a15c13d96d783e360a0f274b527cddc39f99d2
Anuj Gautam <anujgautam@Anujs-MacBook-Air.local>
2024-05-13 14:37:17 +0545

    react commit

---
+++ src/Pages/Home/AboutMe.jsx
@@ -0,0 +1,18 @@
+export default function AboutMe() {
+    return (
+        <section id="AboutMe" className="about--section">
+            <div className="about--section--img">
+                <img src="./img/about-me.jpeg" alt="About Me" />
+            </div>
+            <div className="hero--section--content--box about--section--box">
+                <div className="hero--section--content">
+                    <p className="section--title">About</p>
+                    <h1 className="skills-section--heading">About Me</h1>
+                    <p className="hero--section-description">Hi, I have completed my Bachelors Degree in Computer Engineering and currently in search of a job in web development.
+                    </p>
+                    <p className="hero--section-description">I have completed some projects based on JavaScript's library, ReactJS. </p>
+                </div>
+            </div>
+        </section>
+    );
+}
